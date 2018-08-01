419d50d0ddba0e7cf80e271a71b0af7a98bd6da7
wendell256 <wendell256@gmail.com>
2018-07-31 23:54:45 -0600

    Ejercicio dos finalizado

--- main.cpp
+++ main.cpp
@@ -53,39 +53,74 @@ void ejercicio1(){
 
 //funcion que realiza el ejercicio dos
 void ejercicio2(){
-  int ladoa=0,ladob=0,ladoc=0,hip=0,pitagoras=0;
-  cout<<"ingrese la longitud del lado a: "<<endl;
-  cin>>ladoa;
-  while(ladoa<1){
-    cout<<"Valor no valido. Ingrese longitud de nuevo:";
-    cin>>ladoa;
-  };
-  cout<<"ingrese la longitud del lado b: "<<endl;
-  cin>>ladob;
-  while(ladob<1){
-    cout<<"Valor no valido. Ingrese longitud de nuevo:";
-    cin>>ladoa;
-  };
-  cout<<"ingrese la longitud del lado c: "<<endl;
-  cin>>ladoc;
-  while(ladoc<1){
-    cout<<"Valor no valido. Ingrese longitud de nuevo:";
-    cin>>ladoa;
-  };
-  int hip =fmax(fmax(ladoa,ladob),ladoc);
-  //c²=a²+b² para todo triangulo rectangulo
-  hip_pot = exp(hip,2);
-  a_pot = exp(ladoa,2);
-  b_pot = exp(ladob,2);
-  pitagoras = a_pot + b_pot;
+  double a=0,b=0,c=0,ladoa=0,ladob=0,ladoc=0,hip=0,a_pot=0,b_pot=0,c_pot=0,pitagoras=0;
   
-  if(hip_pot == pitagoras){
+  cout<<"ingrese la longitud del lado a: "<<endl;
+  cin>>a;
+  while(a<1){
+    cout<<"Valor no valido. Ingrese longitud de nuevo:";
+    cin>>a;
+  };
+  
+  cout<<"ingrese la longitud del lado b: "<<endl;
+  cin>>b;
+  while(b<1){
+    cout<<"Valor no valido. Ingrese longitud de nuevo:";
+    cin>>b;
+  };
+  
+  cout<<"ingrese la longitud del lado c: "<<endl;
+  cin>>c;
+  while(c<1){
+    cout<<"Valor no valido. Ingrese longitud de nuevo:";
+    cin>>c;
+  };
+  hip =fmax(a,b);
+  hip = fmax(hip, c);
+  cout<<"hip:"<<hip<<endl;  
+  if(hip == a){
+    ladoa = c;
+    ladoc = hip;
+    ladob = b; 
+  }else if(hip == ladob){
+    ladob = ladoc;
+    ladoc = hip;
+    ladoa = a;
+  }
+  cout<<"ladoa: "<<ladoa<<endl;
+  cout<<"ladob: "<<ladob<<endl;
+  cout<<"ladoc: "<<ladoc<<endl;
+  //c²=a²+b² para todo triangulo rectangulo
+  c_pot = pow(ladoc,2);
+  a_pot = pow(ladoa,2);
+  b_pot = pow(ladob,2);
+  pitagoras = a_pot + b_pot;
+  cout<<"apot: "<<a_pot<<endl;
+  cout<<"bpot: "<<b_pot<<endl;
+  cout<<"cpot: "<<c_pot<<endl;
+  /*α=arccos[(a²-b²-c²)/(-2bc)]
+    β=arccos[(b²-a²-c²)/(-2ac)]
+    γ=arccos[(c²-a²-b²)/(-2ac)]
+  */
+  //c = hip_pot
+  double alpha = (acos((a_pot - b_pot - c_pot)/(-2*ladob*ladoc)))*(180/M_PI);
+  double beta  = (acos((b_pot - a_pot - c_pot)/(-2*ladoa*ladoc)))*(180/M_PI);  
+  double gamma = (acos((c_pot - a_pot - b_pot)/(-2*ladoa*ladoc)))*(180/M_PI);
+
+  /*Area = sqrt[s(s-a)(s-b)(s-c)]
+    s = (a+b+c)/2*/
+  double Sarea = (ladoa + ladob + ladoc)/2;
+  double area = sqrt(Sarea*(Sarea - ladoa)*(Sarea - ladob)*(Sarea - ladoc));
+ 
+  if(c_pot == pitagoras){
     cout<<"El triangulo es un triangulo rectangulo."<<endl;
   }else{
     cout<<"El triangulo no es un triangulo rectangulo."<<endl;
   }//fin if de ver si es triangulo rectangulo
-  
-
+  cout<<"angulo  α = "<<  alpha <<"°"<<endl;
+  cout<<"angulo  β = "<<  beta <<"°"<<endl;
+  cout<<"angulo γ = "<< gamma <<"°"<<endl;
+  cout<<"Area = "<<area<<endl;
 }
 //fin funcion que realiza el ejercicio dos
 
